208a2fcee1dd313239a3c792b3b94508302b0833
davtc <47212365+davtc@users.noreply.github.com>
2022-12-09 01:41:11 +1100

    Added game settings form

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,31 +1,77 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+ }
+
 function App() {
+  const [settingData, setSettingData] = useReducer(formReducer, {
+    size: 3,
+    win: 3,
+    symbol1: "O",
+    symbol2: "X"
+  });
+  const [start, setStart] = useState(false);
+  
+  const handleStart = event => {
+    event.preventDefault();
+    setStart(true);
+    console.log(settingData)
+ 
+    setTimeout(() => {
+      setStart(false);
+    }, 3000)
+  }
+
+  const handleReset = () => {
+    setSettingData({
+      size: 3,
+      win: 3,
+      symbol1: "O",
+      symbol2: "X"
+    });
+  }
+
+  const handleChange = event => {
+    setSettingData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
   return (
     <div className="wrapper">
       <h1>Game Settings</h1>
-      <form>
-        <fieldset>
+      <form onSubmit={handleStart}>
+        <fieldset disabled={start}>
           <label>
-            <p>Select a grid size</p>
-            <input name="size" />
+            <p>Select a grid size between 3 and 15</p>
+            <input type="number" name="size" required step="1" min="3" max="15" onChange={handleChange} value={settingData.size}/>
           </label>
           <label>
-            <p>Select the number of matches in a row to win</p>
-            <input name="win" />
+            <p>Select the number of matches in a row to win between 3 and 5</p>
+            <input type="number" name="win" required step="1" min="3" max="5" onChange={handleChange} value={settingData.win}/>
           </label>
           <label>
             <p>Select the letter to represent player 1 (eg. O)</p>
-            <input name="symbol1" />
+            <input type="text" name="symbol1" required minLength="1" maxLength="1" onChange={handleChange} value={settingData.symbol1}/>
           </label>
           <label>
             <p>Select the letter to represent player 2 (eg. X)</p>
-            <input name="symbol2" />
+            <input type="text" name="symbol2" required minLength="1" maxLength="1" onChange={handleChange} value={settingData.symbol2}/>
           </label>
         </fieldset>
-        <button type="submit">Start Game</button>
+        <div>
+          <button type="submit" disabled={start}>Start Game</button>
+        </div>
       </form>
+      {start &&
+       <div>Loading...</div>
+      }
     </div>
   );
 }
